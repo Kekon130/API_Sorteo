2c7686affa0d8d340d006d50689cf5338ce70b3f
Lolo20020803 <alexsp2002@outlook.es>
2022-11-09 16:06:10 +0100

    mend
    venta de boletos

--- utils/app.js
+++ utils/app.js
@@ -3,6 +3,7 @@ const { PrismaClient } = require('@prisma/client')
 
 const sellerPath= require('../routes/sellerRoutes');
 const ticketPath = require('../routes/ticketRoute');
+const clientPath = require('../routes/clientRoutes');
 
 const app = express();
 
@@ -13,8 +14,9 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/seller',sellerPath);
 app.use('/ticket',ticketPath);
+app.use('/client',clientPath);
 
-app.use('/',(req,res)=>{
+app.use('/',({},res)=>{
     res.send('Api Sorteo Core Navidad');
 })
 
